fc57b0f793f7d689354e602ca45e5231d05ffdad
Tri Vo <tri.vo@fdssoft.com>
2021-04-19 12:04:15 +0700

    Init trezor

--- public/build.js
+++ public/build.js
@@ -284,6 +284,8 @@ function initialize() {
     mainWindow = new BrowserWindow(windowOptions)
     mainWindow.setMenu(null)
     mainWindow.loadURL(loadUrlPath)
+    mainWindow.toggleDevTools()
+
 
     mainWindow.once('ready-to-show', () => {
       mainWindow.show()
